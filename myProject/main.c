0c3aa8475879f1947c3dc6295b8d1d026216b890
hyochangsung <xx1733@naver.com>
2023-03-22 17:54:35 +0900

    Initial Commit

---
+++ myProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  myProject
+//
+//  Created by 성효창 on 2023/03/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
